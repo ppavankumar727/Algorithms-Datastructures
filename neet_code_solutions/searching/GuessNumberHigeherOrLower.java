5d271442b2140e75b453985c661044b918dc7d95
ppavankumar727 <ppavankumar727@gmail.com>
2022-03-27 15:37:10 +0530

    added comments

--- neet_code_solutions/searching/GuessNumberHigeherOrLower.java
+++ neet_code_solutions/searching/GuessNumberHigeherOrLower.java
@@ -1,3 +1,20 @@
+/*source = https://leetcode.com/problems/guess-number-higher-or-lower/
+374. Guess Number Higher or Lower
+Easy
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+*/
 class GuessNumberHigherOrLower{
     public static int pick =  1702766719;
     public static void main(String []args){
